9d7164967fdf768f1f369b611b2a23d302a4e463
Pouria-ye-Vali <pdevopseng@gmail.com>
2018-07-06 11:09:27 +0430

    commited

--- main.js
+++ main.js
@@ -29,12 +29,15 @@ function saveIssue(e) {
   
   e.preventDefault(); 
 }
-
-
-
-
-
-
+function setStatusClosed (id) {
+  var issues = JSON.parse(localStorage.getItem('issues'));
+  
+  for(var i = 0; i < issues.length; i++) {
+    if (issues[i].id == id) {
+      issues[i].status = "Closed";
+    }
+  }
+  
 function fetchIssues () {
     var issues = JSON.parse(localStorage.getItem('issues'));
     var issuesList = document.getElementById('issuesList');
